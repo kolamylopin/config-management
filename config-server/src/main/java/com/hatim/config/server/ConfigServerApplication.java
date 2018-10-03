13266efe1e16298cbe5e98dffbc356b346e2fc66
Hatim <hatim@mypc.com>
2018-10-03 06:50:12 +0100

    Added docker compose

--- config-server/src/main/java/com/hatim/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/hatim/config/server/ConfigServerApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigServerApplication {
+	private final static Logger logger = Logger.getLogger(ConfigServerApplication.class.getName());
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
