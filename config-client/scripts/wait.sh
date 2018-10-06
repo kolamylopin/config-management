7bff3dba87bd1bf3cffc3c402dfed2649303cc5f
Hatim <hatim@mypc.com>
2018-10-06 20:48:13 +0100

    Added startup sync

--- config-client/scripts/wait.sh
+++ config-client/scripts/wait.sh
@@ -4,7 +4,7 @@ path="$1"
 cmd="$2"
 
 until $(curl --output /dev/null --silent --head --fail $path); do
-    printf "waiting for config server $path\n"
+    printf "waiting for $path\n"
     sleep 5
 done
 
